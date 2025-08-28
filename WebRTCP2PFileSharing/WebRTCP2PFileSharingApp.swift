6856251a18a4b2b3d1365f9bb3f2422f6b10351e
Raguraman <raguraman.asokan@gmail.com>
2025-08-28 12:05:39 +0300

    Initial Commit

---
+++ WebRTCP2PFileSharing/WebRTCP2PFileSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTCP2PFileSharingApp.swift
+//  WebRTCP2PFileSharing
+//
+//  Created by Raguraman on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WebRTCP2PFileSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
